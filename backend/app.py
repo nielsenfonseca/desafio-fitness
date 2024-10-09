b098b74279d81436a5de1f5dc5a39db9d38918de
Nielsen Fonseca <nielsen.mfj@gmail.com>
2024-10-09 18:50:07 -0300

    Preparando deploy no Heroku

--- backend/app.py
+++ backend/app.py
@@ -2,20 +2,21 @@ from flask import Flask, session
 from backend.routes import configure_routes
 from backend.database import init_db
 from backend.models import db
+from config import get_config  # Importa a função que define o ambiente
 
 app = Flask(__name__)
 
-# Configurações da aplicação
-app.config.from_object('config')
+# Configurações da aplicação com base no ambiente (desenvolvimento ou produção)
+app.config.from_object(get_config())
 
 # Necessário para as sessões
-app.config['SECRET_KEY'] = '2ebfcc42fdd537ac1424f6a28bda320e'  # Uma chave segura
+app.config['SECRET_KEY'] = app.config.get('SECRET_KEY', '2ebfcc42fdd537ac1424f6a28bda320e')  # Usa a chave do config ou o valor padrão
 
 # Inicializa o banco de dados e as rotas
 init_db(app)
 configure_routes(app)
 
-# Cria todas as tabelas
+# Cria todas as tabelas (caso ainda não existam)
 with app.app_context():
     db.create_all()
 
--- config.py
+++ config.py
@@ -1,4 +1,20 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///fitness.db'  # Banco local para desenvolvimento
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '2ebfcc42fdd537ac1424f6a28bda320e'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    # Banco de dados local para desenvolvimento
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///fitness.db'
+
+class ProductionConfig(Config):
+    # Banco de dados Postgres no Supabase para produção
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.tpavwcyenfhucopzthrm:Stihels2009!@aws-0-sa-east-1.pooler.supabase.com:6543/postgres'
+
+# Função que seleciona a configuração com base na variável de ambiente
+def get_config():
+    env = os.getenv('FLASK_ENV', 'development')  # 'development' é o padrão
+    if env == 'production':
+        return ProductionConfig
+    return DevelopmentConfig
